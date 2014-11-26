a90b0408e1f0867993841e54537d4ca5bd132f29
huangwei <huangwei@test15169x.ops.corp.qihoo.net>
2014-11-26 16:17:05 +0800

    up

--- install.sh
+++ install.sh
@@ -8,15 +8,24 @@ fi
 ln -sf "$PWD/vim" ~/.vim
 ln -sf "$PWD/vim/vimrc" ~/.vimrc
 
-# link zsh files
-ln -sf "$PWD/zsh/zshrc" ~/.zshrc
-
 # link z.sh files
 ln -sf "$PWD/z/z.sh" ~/.z.sh
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s /bin/zsh
-# source new zsh profile
-source ~/.zshrc
+which zsh
+# 安装了zsh
+if [[ $0 == 0 ]]; then
+    # link zsh files
+    ln -sf "$PWD/zsh/zshrc" ~/.zshrc
+
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    chsh -s /bin/zsh
+    # source new zsh profile
+    source ~/.zshrc
+else
+    # link bash files
+    ln -sf "$PWD/bash/bash_profile" ~/.bash_profile
+    source ~/.bash_profile
+fi
+
 
 
